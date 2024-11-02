c419c0cb333e443b9b62f45a84afe7df28a1e318
ClaudiaFue <89424084+ClaudiaFue@users.noreply.github.com>
2024-11-02 18:08:31 -0500

    Update app.py
    Updating when following Video 3 to add the posts from Index.html

--- app.py
+++ app.py
@@ -22,11 +22,17 @@ def get_db_connection():
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
 def index():
+     #get a connection to the database
+    conn = get_db_connection()
+    #execute a query to read all posts from the posts table in the database
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    #close the connection
+    conn.close()
+    #send the posts to the index.html template to be displayed
     
-    return "<h1>Welcome to Claudia's Blog</h1>"
+    return render_template('index.html', posts=posts)    
 
 
 # route to create a post
-
 
 app.run(port=5008)
